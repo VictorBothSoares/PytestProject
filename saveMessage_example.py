348fdfb3b949234dd12884068ae05ccecfe63d00
VictorBothSoares <127154736+VictorBothSoares@users.noreply.github.com>
2023-03-27 21:38:15 -0300

    Versão 1.0

--- saveMessage_example.py
+++ saveMessage_example.py
@@ -3,8 +3,8 @@ from cryptographyFramework import *
 initializeWrite()
 user = "Fulano"
 password = "1234"
-encryptedText = encryptMessage(user, password, "Minha mensagem secreta!")
+encryptedText = encryptMessage(user, password, "Não aguento mais aaaaaaaaaaaa")
 saveNewLine(encryptedText)
-encryptedText = encryptMessage(user, password, "Minha segunda mensagem secreta!")
+encryptedText = encryptMessage(user, password, "Não aguento mais aaaaaaaaaaaa!")
 saveNewLine(encryptedText)
 
